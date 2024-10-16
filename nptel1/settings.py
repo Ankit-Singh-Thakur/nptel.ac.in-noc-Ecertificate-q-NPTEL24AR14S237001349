df5d2e524b4720bd2adcd62f0ebbedbd0609632a
ankit <ankitsinghthakur253@gmail.com>
2024-10-16 12:17:57 +0530

    first commit

--- nptel1/settings.py
+++ nptel1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t#8u@y52lwnbiui_2s4t&ffrk)ou31&#luz(%($9u!5a(5r5w9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
